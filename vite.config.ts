bfec73fa17101a1f90a9192dc07e5c3feeb98529
rishikesh-integration <rishikesh.bhise@gmail.com>
2026-03-24 19:53:13 +0530

    to merge

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      // Manually point "xlsx" to the actual library file in node_modules
+      xlsx: path.resolve(__dirname, 'node_modules/xlsx/xlsx.mjs'),
+    },
+  },
+  optimizeDeps: {
+    include: ['xlsx'],
+  },
+});
