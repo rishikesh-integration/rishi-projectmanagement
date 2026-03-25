71eb28c8d8ed8fd83cd1c4892e4ddbccf6e71ea2
Rishikesh Bhise <rishikeshbhise@Rishikeshs-MacBook-Pro.local>
2026-03-25 10:43:49 +0530

    Fix firebase config, update packages and all latest changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      // Manually point "xlsx" to the actual library file in node_modules
-      xlsx: path.resolve(__dirname, 'node_modules/xlsx/xlsx.mjs'),
-    },
-  },
   optimizeDeps: {
     include: ['xlsx'],
+    exclude: []
   },
-});
+  resolve: {
+    mainFields: ['main', 'module']
+  }
+})
